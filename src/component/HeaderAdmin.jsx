39a0819cabc7ef48d307ceb0d516f15e567f9523
Rémi Le Gall <126484877+Vanbecker@users.noreply.github.com>
2023-08-01 17:29:23 +0200

    login

--- src/component/HeaderAdmin.jsx
+++ src/component/HeaderAdmin.jsx
@@ -1,19 +1,50 @@
+// import { Link } from "react-router-dom";
+
+// const HeaderAdmin = () => {
+//     return (
+//         <header>
+//             <nav>
+//                 <ul>
+//                     <li>
+//                         <Link to={"/admin/coworkings"}>Liste des coworkings</Link>
+//                     </li>
+//                     <li>
+//                         <Link to={"/admin/coworkings/create"}>Ajouter un coworking</Link>
+//                     </li>
+//                 </ul>
+//             </nav>
+//         </header>
+//     );
+// };
+
+// export default HeaderAdmin;
+
+/////
+
+import React from "react";
 import { Link } from "react-router-dom";
 
 const HeaderAdmin = () => {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={"/admin/coworkings"}>Liste des coworkings</Link>
-                    </li>
-                    <li>
-                        <Link to={"/admin/coworkings/create"}>Ajouter un coworking</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <div className="container d-flex justify-content-center mt-4">
+            <div className="card">
+                <div className="card-header">
+                    <h3>Page Administrateur</h3>
+                </div>
+                <div className="card-body">
+                    <nav>
+                        <ul className="list-group">
+                            <li className="list-group-item">
+                                <Link to={"/admin/coworkings"}>Liste des coworkings</Link>
+                            </li>
+                            <li className="list-group-item">
+                                <Link to={"/admin/coworkings/create"}>Ajouter un coworking</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,6 +1,13 @@
+
+
+import React from "react";
 import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+
+    const navigate = useNavigate();
+
     const handleLoginSubmit = async (event) => {
         event.preventDefault();
 
@@ -12,37 +19,46 @@ const LoginPage = () => {
             headers: {
                 "Content-Type": "application/json",
             },
-
             body: JSON.stringify({ username, password }),
         });
 
-        // si la réponse est valide
+        // Si la réponse est valide
         if (loginResponse.status === 200) {
             const loginData = await loginResponse.json();
 
-            // je récupère le jwt dans le data
+            // Récupérer le jwt dans le data
             const jwt = loginData.data;
 
-            // je stocke le jwt dans un cookie
+            // Stocker le jwt dans un cookie
             Cookies.set("jwt", jwt);
+
+            navigate("/admin/coworkings");
+
         }
     };
 
     return (
-        <form onSubmit={handleLoginSubmit}>
-            <div>
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" />
-            </div>
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <form onSubmit={handleLoginSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">Nom d'utilisateur</label>
+                            <input type="text" className="form-control" name="username" />
+                        </div>
 
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" />
-            </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Mot de passe</label>
+                            <input type="password" className="form-control" name="password" />
+                        </div>
 
-            <input type="submit" />
-        </form>
+                        <button type="submit" style={{ backgroundColor: "#FFB8BD", borderColor: "#FFB8BD", color: "white" }} className="btn btn-primary">Se connecter</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
 
 export default LoginPage;
+
