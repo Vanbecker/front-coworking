95aa59a89d03d111a3b0211a82bf5d2fb10825a6
Rémi Le Gall <126484877+Vanbecker@users.noreply.github.com>
2023-08-02 15:39:09 +0200

    verificationadmin

--- src/component/admin/HeaderAdmin.jsx
+++ src/component/admin/HeaderAdmin.jsx
@@ -1,44 +1,68 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import Cookies from "js-cookie"; // N'oubliez pas d'importer le module Cookies
+// import React from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import Cookies from "js-cookie"; // N'oubliez pas d'importer le module Cookies
 
-const HeaderAdmin = () => {
-    const navigate = useNavigate();
+// const HeaderAdmin = () => {
+//     const navigate = useNavigate();
+//     ////
+//     useEffect(() => {
+//         const jwt = Cookies.get("jwt");
 
-    const handleLogout = () => {
-        Cookies.remove("jwt");
-        navigate("/login");
-    };
+//         // S'il n'existe pas, cela signifie que l'utilisateur n'est pas connecté
+//         // On le redirige vers la page de login
+//         if (!jwt) {
+//             navigate("/login");
+//         }
 
-    return (
-        <div className="container d-flex justify-content-center mt-4">
-            <div className="card">
-                <div className="card-header">
-                    <h3>Page Administrateur</h3>
-                </div>
-                <div className="card-body">
-                    <nav>
-                        <ul className="list-group">
-                            <li className="list-group-item">
-                                <Link style={{ color: "#FFB8BD" }} to={"/admin/coworkings"}>Liste des coworkings</Link>
-                            </li>
-                            <li className="list-group-item" >
-                                <Link style={{ color: "#FFB8BD" }} to={"/admin/createcoworking"}>Ajouter un coworking</Link>
-                            </li>
-                            <li className="list-group-item">
-                                <a href="#" className="link-danger" onClick={handleLogout}>
-                                    Se déconnecter
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    );
-};
+//         // On décode le jwt
+//         const user = jwtDecode(jwt);
 
-export default HeaderAdmin;
+//         // Si l'utilisateur a le rôle user (ID 1)
+//         // On le redirige vers l'accueil public
+//         if (user.data.role === 1) {
+//             navigate("/");
+//         }
+//     }, []);
+//     ////
+
+//     const handleLogout = () => {
+//         Cookies.remove("jwt");
+//         navigate("/login");
+//     };
+
+//     return (
+//         <div className="container d-flex justify-content-center mt-4">
+//             <div className="card">
+//                 <div className="card-header">
+//                     <h3>Page Administrateur</h3>
+//                 </div>
+//                 <div className="card-body">
+//                     <nav>
+//                         <ul className="list-group">
+//                             <li className="list-group-item">
+//                                 <Link style={{ color: "#FFB8BD" }} to={"/admin/coworkings"}>Liste des coworkings</Link>
+//                             </li>
+//                             <li className="list-group-item" >
+//                                 <Link style={{ color: "#FFB8BD" }} to={"/admin/createcoworking"}>Ajouter un coworking</Link>
+//                             </li>
+//                             <li className="list-group-item">
+//                                 <a href="#" className="link-danger" onClick={handleLogout}>
+//                                     Se déconnecter
+//                                 </a>
+//                             </li>
+//                         </ul>
+//                     </nav>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default HeaderAdmin;
+
+
+//////
+
 
 // import Cookies from "js-cookie";
 // import jwtDecode from "jwt-decode";
@@ -89,3 +113,69 @@ export default HeaderAdmin;
 // };
 
 // export default HeaderAdmin;
+
+
+/////
+
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
+
+const HeaderAdmin = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const jwt = Cookies.get("jwt");
+
+        // S'il n'existe pas, cela signifie que l'utilisateur n'est pas connecté
+        // On le redirige vers la page de login
+        if (!jwt) {
+            navigate("/login");
+        }
+
+        // On décode le jwt
+        const user = jwtDecode(jwt);
+
+        // Si l'utilisateur a le rôle user (ID 1)
+        // On le redirige vers l'accueil public
+        if (user.data.role === 2) {
+            navigate("/");
+        }
+    }, []);
+
+    const handleLogout = () => {
+        Cookies.remove("jwt");
+        navigate("/login");
+    };
+
+    return (
+        <div className="container d-flex justify-content-center mt-4">
+            <div className="card">
+                <div className="card-header">
+                    <h3>Page Administrateur</h3>
+                </div>
+                <div className="card-body">
+                    <nav>
+                        <ul className="list-group">
+                            <li className="list-group-item">
+                                <Link style={{ color: "#FFB8BD" }} to={"/admin/coworkings"}>Liste des coworkings</Link>
+                            </li>
+                            <li className="list-group-item" >
+                                <Link style={{ color: "#FFB8BD" }} to={"/admin/createcoworking"}>Ajouter un coworking</Link>
+                            </li>
+                            <li className="list-group-item">
+                                <a href="#" className="link-danger" onClick={handleLogout}>
+                                    Se déconnecter
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeaderAdmin;
+
